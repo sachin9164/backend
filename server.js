9df6445651b6831556c2d1c08357aefd0e3fa8b7
Sachin <sachinkbcm@gmail.com>
2021-08-05 20:53:02 +0530

    made change

--- server.js
+++ server.js
@@ -7,15 +7,7 @@ const uri = 'mongodb+srv://sachin:LL5lrSQfCaOYdGOs@cluster0.6s9rx.mongodb.net/bm
 const app = express();
 app.use(cors());
 app.use(express.json());
-mongoose.connect(uri,{ useNewUrlParser: true , useUnifiedTopology: true }).then(()=>
-{
-    app.listen( process.env.PORT || 5000,()=>{
-        console.log("Porting running in 5000 !")
-    });
-    console.log("Connected to DB")
-}).catch((error)=>{
-    console.log(error)
-})
+
 const usersRouter = require('./routes/user');
 
 app.use('/users', usersRouter);
@@ -26,17 +18,27 @@ app.get('/',(req,res)=>{
     
 })
 
-app.use(express.static(path.join(__dirname, "./frontend/build")));
+if ( process.env.NODE_ENV == "production"){
 
-app.get("*", function (_, res) {
-  res.sendFile(
-    path.join(__dirname, "./frontend/build/index.html"),
-    function (err) {
-      if (err) {
-        res.status(500).send(err);
-      }
-    }
-  );
-});
+    app.use(express.static("frontend/build"));
+
+    const path = require("path");
+
+    app.get("*", (req, res) => {
+
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+
+    })
 
 
+}
+
+mongoose.connect(uri,{ useNewUrlParser: true , useUnifiedTopology: true }).then(()=>
+{
+    app.listen( process.env.PORT || 5000,()=>{
+        console.log("Porting running in 5000 !")
+    });
+    console.log("Connected to DB")
+}).catch((error)=>{
+    console.log(error)
+})
