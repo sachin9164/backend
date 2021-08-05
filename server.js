a6b29218d331f32ebcf281b507338cc32f4c8ffe
Sachin <sachinkbcm@gmail.com>
2021-08-05 20:37:20 +0530

    made change

--- server.js
+++ server.js
@@ -26,19 +26,17 @@ app.get('/',(req,res)=>{
     
 })
 
-if ( process.env.NODE_ENV == "production"){
+app.use(express.static(path.join(__dirname, "./frontend/build")));
 
-    app.use(express.static("client/build"));
-
-    const path = require("path");
-
-    app.get("*", (req, res) => {
-
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-
-    })
-
-
-}
+app.get("*", function (_, res) {
+  res.sendFile(
+    path.join(__dirname, "./frontend/build/index.html"),
+    function (err) {
+      if (err) {
+        res.status(500).send(err);
+      }
+    }
+  );
+});
 
 
